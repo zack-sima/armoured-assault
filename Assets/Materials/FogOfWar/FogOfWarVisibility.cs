9918a647e4138b8d27023147a169301a1b6a4b72
Zack Sima <68410154+comrade-normie@users.noreply.github.com>
2021-08-21 19:05:56 -0700

    Add files via upload

---
+++ Assets/Materials/FogOfWar/FogOfWarVisibility.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogOfWarVisibility : MonoBehaviour
+{
+	private bool observed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update()
+	{
+		if(observed)
+		{
+			GetComponent<Renderer>().enabled = true;
+		}
+		else
+		{
+			GetComponent<Renderer>().enabled = false;
+		}
+
+		observed = false;
+	}
+	
+	void Observed()
+	{
+		Debug.Log("Observed",gameObject);
+		observed = true;
+	}
+}
